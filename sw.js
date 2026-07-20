4bc86daa789b3f0621c5168004070b6cb9c7a376
canserkanozcan-jpg <canserkanozcan@gmail.com>
2026-07-20 09:33:20 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='mml-fleet-v2';const ASSETS=['./','./index.html','./fleet-data.js','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='mml-fleet-20260720-tcluk-logo';const ASSETS=['./','./index.html','./fleet-data.js','./manifest.json,
+  "mml-aircraft-logo.png",
+  "icon-192.png",
+  "icon-512.png"
+]];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
