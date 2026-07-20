04cccba3f88bb48f6723508048a1587c49c7468e
canserkanozcan-jpg <canserkanozcan@gmail.com>
2026-07-20 09:42:29 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,38 @@
-const CACHE='mml-fleet-20260720-tcluk-logo';const ASSETS=['./','./index.html','./fleet-data.js','./manifest.json,
-  "mml-aircraft-logo.png",
-  "icon-192.png",
-  "icon-512.png"
-]];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE = 'mml-fleet-20260720-tcluk-logo-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './fleet-data.js',
+  './fleet-data.json',
+  './manifest.json',
+  './mml-aircraft-logo.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
